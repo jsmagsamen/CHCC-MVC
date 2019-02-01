ad250e967ce6f27b77e0c350d5e73a5c1227ca9a
Jason Magsamen <jsmasc@acfwad.acfw.in.us>
2019-02-01 11:41:16 -0500

    Add project files.

---
+++ CHCC/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CHCC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Beliefs()
+        {
+            ViewBag.Message = "Your beliefs page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Location()
+        {
+            ViewBag.Message = "Your location page.";
+
+            return View();
+        }
+
+        public ActionResult Pictures()
+        {
+            ViewBag.Message = "Your pictures page.";
+
+            return View();
+        }
+    }
+}
